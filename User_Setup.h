369b3d436ee1e7984382a6a35c1ed21f2e3d08c2
Afantor <13148946269@163.com>
2019-07-02 21:12:20 +0800

    change esp32 pin for TFT.

--- User_Setup.h
+++ User_Setup.h
@@ -162,12 +162,12 @@
 #define TFT_SCLK 14
 #define TFT_CS   15  // Chip select control pin
 #define TFT_DC    2  // Data Command control pin
-#define TFT_RST   27  // Reset pin (could connect to RST pin)
+#define TFT_RST   4  // Reset pin (could connect to RST pin)
 //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 
-//#define TFT_BL   32  // LED back-light (only for ST7789 with backlight control pin)
+#define TFT_BL   -1  // LED back-light (only for ST7789 with backlight control pin)
 
-//#define TOUCH_CS 21     // Chip select pin (T_CS) of touch screen
+#define TOUCH_CS 32     // Chip select pin (T_CS) of touch screen
 
 //#define TFT_WR 22    // Write strobe for modified Raspberry Pi TFT only
 
@@ -265,7 +265,7 @@
 // The ESP32 has 2 free SPI ports i.e. VSPI and HSPI, the VSPI is the default.
 // If the VSPI port is in use and pins are not accessible (e.g. TTGO T-Beam)
 // then uncomment the following line:
-//#define USE_HSPI_PORT
+#define USE_HSPI_PORT
 
 // Comment out the following #define if "SPI Transactions" do not need to be
 // supported. When commented out the code size will be smaller and sketches will
